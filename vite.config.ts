e6371c290b78eacea693c1d0c1c637a1214ef887
jahnaviamballa-jpg <jahnaviamballa@gmail.com>
2025-12-13 21:58:54 +0530

    error fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.string(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringi(env.gemini__API_KEY)
       },
       resolve: {
         alias: {
