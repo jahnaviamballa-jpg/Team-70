c67d502395e800553ed72a6728459376722e840f
jahnaviamballa-jpg <jahnaviamballa@gmail.com>
2025-12-13 23:06:04 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 dotenv.config();
 
 const app = express();
-const PORT = processes.env.PORT || 3000;
+const PORT = processes.env.PORT || 3000; 
 
 app.use(cors());
 app.use(express.json());
